fbca83cdfd0db234e8b8b7817a4fb8a39fe538e7
Jack Shultz <jackygrahamez@gmail.com>
2015-04-25 21:15:43 -0400

    updated build

--- public/dist/application.js
+++ public/dist/application.js
@@ -547,6 +547,10 @@ angular.module('users').config(['$stateProvider',
 	function($stateProvider) {
 		// Users state routing
 		$stateProvider.
+		state('dashboard', {
+			url: '/dashboard',
+			templateUrl: 'modules/users/views/dashboard.client.view.html'
+		}).
 		state('profile', {
 			url: '/settings/profile',
 			templateUrl: 'modules/users/views/settings/edit-profile.client.view.html'
@@ -619,6 +623,34 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 		};
 	}
 ]);
+'use strict';
+
+angular.module('core').controller('DashboardController',
+['$scope', 'Authentication', 'Menus', '$location',
+	function($scope, Authentication, Menus, $location) {
+		$scope.authentication = Authentication;
+		$scope.contacts = [{},{},{}];
+		$scope.serviceProvider = [
+			{name: "I Don't Know", domain: ""},
+			{name: "AT&T", domain: "txt.att.net"},
+			{name: "T-Mobile", domain: "tmomail.net"},
+			{name: "Verizon", domain: "vtext.com"},
+			{name: "Sprint", domain: "messaging.sprintpcs.com"},
+			{name: "Virgin Mobile", domain: "vmobl.com"},
+			{name: "Tracfone", domain: "mmst5.tracfone.com"},
+			{name: "Metro PCS", domain: "mymetropcs.com"},
+			{name: "Boost Mobile", domain: "myboostmobile.com"},
+			{name: "Cricket", domain: "sms.mycricket.com"},
+			{name: "Nextel", domain: "messaging.nextel.com"},
+			{name: "Alltel", domain: "message.alltel.com"},
+			{name: "Ptel", domain: "ptel.com"},
+			{name: "Suncom", domain: "number@tms.suncom.com"},
+			{name: "Qwest", domain: "qwestmp.com"},
+			{name: "U.S. Cellular", domain: "number@email.uscc.net"}
+		];
+	}
+]);
+
 'use strict';
 
 angular.module('users').controller('PasswordController', ['$scope', '$stateParams', '$http', '$location', 'Authentication',
