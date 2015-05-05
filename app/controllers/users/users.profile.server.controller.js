585cb4ccb3aedfeeb6b8f2710273e2816fba483c
Jack Shultz <jackygrahamez@gmail.com>
2015-05-04 21:23:05 -0400

    updated mayday to textsosalert

--- app/controllers/users/users.profile.server.controller.js
+++ app/controllers/users/users.profile.server.controller.js
@@ -7,7 +7,10 @@ var _ = require('lodash'),
 	errorHandler = require('../errors.server.controller.js'),
 	mongoose = require('mongoose'),
 	passport = require('passport'),
-	User = mongoose.model('User');
+	User = mongoose.model('User'),
+	mandrill = require('mandrill-api/mandrill');
+
+var mandrill_client = new mandrill.Mandrill('Q6k4Qnx2hJ-JPJ_AlCluyw');
 
 function checkUnique(obj, list) {
 	for (var i = 0; i < list.length; i++) {
@@ -18,6 +21,96 @@ function checkUnique(obj, list) {
 	return true;
 }
 
+function sendMessage() {
+	var d = new Date(),
+		date = d.toUTCString(),
+		message = {
+	    "html": "<p>Example HTML content</p>",
+	    "text": "Example text content",
+	    "subject": "example subject",
+	    "from_email": "message.from_email@example.com",
+	    "from_name": "Example Name",
+	    "to": [{
+	            "email": "john.g.shultz@gmail.com",
+	            "name": "Recipient Name",
+	            "type": "to"
+	        }],
+	    "headers": {
+	        "Reply-To": "message.reply@example.com"
+	    },
+	    "important": false,
+	    "track_opens": null,
+	    "track_clicks": null,
+	    "auto_text": null,
+	    "auto_html": null,
+	    "inline_css": null,
+	    "url_strip_qs": null,
+	    "preserve_recipients": null,
+	    "view_content_link": null,
+	    "bcc_address": "message.bcc_address@example.com",
+	    "tracking_domain": null,
+	    "signing_domain": null,
+	    "return_path_domain": null,
+	    "merge": true,
+	    "merge_language": "mailchimp",
+	    "global_merge_vars": [{
+	            "name": "merge1",
+	            "content": "merge1 content"
+	        }],
+	    "merge_vars": [{
+	            "rcpt": "recipient.email@example.com",
+	            "vars": [{
+	                    "name": "merge2",
+	                    "content": "merge2 content"
+	                }]
+	        }],
+	    "tags": [
+	        "password-resets"
+	    ],
+	    "subaccount": "customer-123",
+	    "google_analytics_domains": [
+	        "example.com"
+	    ],
+	    "google_analytics_campaign": "message.from_email@example.com",
+	    "metadata": {
+	        "website": "www.example.com"
+	    },
+	    "recipient_metadata": [{
+	            "rcpt": "john.g.shultz@gmail.com",
+	            "values": {
+	                "user_id": 123456
+	            }
+	        }],
+	    "attachments": [{
+	            "type": "text/plain",
+	            "name": "myfile.txt",
+	            "content": "ZXhhbXBsZSBmaWxl"
+	        }],
+	    "images": [{
+	            "type": "image/png",
+	            "name": "IMAGECID",
+	            "content": "ZXhhbXBsZSBmaWxl"
+	        }]
+	};
+	var async = false;
+	var ip_pool = "Main Pool";
+	var send_at = date;
+	mandrill_client.messages.send({"message": message, "async": async, "ip_pool": ip_pool}, function(result) {
+	    console.log(result);
+	    /*
+	    [{
+	            "email": "recipient.email@example.com",
+	            "status": "sent",
+	            "reject_reason": "hard-bounce",
+	            "_id": "abc123abc123abc123abc123abc123"
+	        }]
+	    */
+	}, function(e) {
+	    // Mandrill returns the error as an object with name and message keys
+	    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+	    // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+	});
+}
 
 /**
 * Add user's contacts
@@ -48,6 +141,7 @@ exports.addcontact = function(req, res) {
 										res.send({
 											message: 'Contact added successfully'
 										});
+										sendMessage();
 									}
 								});
 							}
