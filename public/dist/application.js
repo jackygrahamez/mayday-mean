65ef5aad8bc6ca0cd6cd3c2df399332342315ff9
Jack Shultz <jackygrahamez@gmail.com>
2015-05-15 00:14:28 -0400

    reverted recaptcha

--- public/dist/application.js
+++ public/dist/application.js
@@ -626,42 +626,10 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 'use strict';
 
 angular.module('core').controller('DashboardController',
-['$scope', '$http', 'Users', 'Authentication', 'Menus', '$location', 'vcRecaptchaService',
-	function($scope, $http, Authentication, Users, Menus, $location, vcRecaptchaService ) {
+['$scope', '$http', 'Users', 'Authentication', 'Menus', '$location',
+	function($scope, $http, Authentication, Users, Menus, $location ) {
 		//reCAPTCHA.setPublicKey('6LfsoAYTAAAAALXOlBeMDzOjDDO0dLeURcsSEzQq');
-		console.log("this is your app's controller");
-		$scope.response = null;
-		$scope.widgetId = null;
-		$scope.model = {
-				key: '6LfsoAYTAAAAALXOlBeMDzOjDDO0dLeURcsSEzQq'
-		};
-		$scope.setResponse = function (response) {
-				console.info('Response available');
-				$scope.response = response;
-		};
-		$scope.setWidgetId = function (widgetId) {
-				console.info('Created widget ID: %s', widgetId);
-				$scope.widgetId = widgetId;
-		};
-		$scope.submit = function () {
-				var valid;
-				/**
-				* SERVER SIDE VALIDATION
-				*
-				* You need to implement your server side validation here.
-				* Send the reCaptcha response to the server and use some of the server side APIs to validate it
-				* See https://developers.google.com/recaptcha/docs/verify
-				*/
-				console.log('sending the captcha response to the server', $scope.response);
-				if (valid) {
-						console.log('Success');
-				} else {
-						console.log('Failed validation');
-						// In case of a failed validation you need to reload the captcha
-						// because each response can be checked just once
-						vcRecaptchaService.reload($scope.widgetId);
-				}
-		};
+
 
 		$scope.user = Authentication.user;
 		$scope.authentication = Authentication;
@@ -738,7 +706,7 @@ angular.module('core').controller('DashboardController',
 					}).error(function(response) {
 						console.log('error: '+response.message);
 						$scope.error = response.message;
-					});					
+					});
 			} else {
 					console.log('Failed validation');
 					// In case of a failed validation you need to reload the captcha
